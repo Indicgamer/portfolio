3961e38592b2dc93cadab3d3d675840bbd884c8e
indic_gamer <channayyagarampalli@gmmail.com>
2024-01-17 01:17:08 +0530

    fix autocomplete problem

--- src/sections/ContactMe.jsx
+++ src/sections/ContactMe.jsx
@@ -9,15 +9,15 @@ export const ContactMe = () => {
         <form action="mailto:indicgamer@gmail.com" method="post" className="form flex flex-col gap-10 px-5 py-5">
         <div className="fname flex gap-3 ">
         <label htmlFor="name" className='text-lg sm:text-xl text-pink-600 font-palanquin font-bold'>First Name: </label>
-        <input type="text" id='name' className='focus-visible:outline-pink-600' />
+        <input type="text" id='name' className='focus-visible:outline-pink-600' autoComplete='on' />
         </div>
         <div className="lname flex gap-3">
           <label htmlFor="name" className='text-lg sm:text-xl text-pink-600 font-palanquin font-bold'>Last Name: </label>
-          <input type="text" id='name' className='focus-visible:outline-pink-600' />
+          <input type="text" id='name' className='focus-visible:outline-pink-600' autoComplete='on'  />
         </div>
         <div className='w-full flex flex-col gap-5'>
           <label htmlFor="desc" className='text-lg sm:text-xl text-pink-600 font-palanquin font-bold'>Message: </label>
-          <textarea name="Message" id="desc" cols="30" rows="8" className='focus-visible:outline-pink-600'></textarea>
+          <textarea name="Message" id="desc" cols="30" rows="8" className='focus-visible:outline-pink-600' autoComplete='on' ></textarea>
         </div>
 
         <div>

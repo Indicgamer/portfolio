8019d8a4d6811b9d3d994e589e4f48742b2d94c6
Indicgamer <channayyagarampalli@gmail.com>
2024-08-07 13:24:31 +0530

    project changes

--- src/components/GridElement.jsx
+++ src/components/GridElement.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export const GridElement = ({imgURL,projectTitle,projURL}) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+  const mouseEnterHandler = () => {
+    setIsHovered(true);
+  }
+  const mouseLeaveHandler = () => {
+    setIsHovered(false);
+  }
   return (
-    <div className="hover:scale-110 hover:shadow-lg hover:cursor-pointer ">
-          <img src={imgURL} alt="img" className="w-full h-full object-fill"/>
-          <a href={projURL}>
-          <div className="relative bottom-[100%] z-10 w-full h-full bg-pink-100 opacity-0 hover:opacity-80 flex flex-col justify-center items-center">
-            <h1 className="text-pink-600  font-montserrat font-black text-2xl text-center">{projectTitle}</h1>
-          </div>
-          </a>
-        </div>
+    <div  className="hover:scale-110 hover:shadow-2xl hover:cursor-pointer" onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler} >
+      <a href={projURL}>
+        <img src={imgURL} alt="img"   />
+      </a>
+      <div className={`'z-10 relative bottom-[106px] h-full bg-pink-200 flex-col justify-center items-center  ${isHovered?'flex':'hidden'} '`}>
+        <h1 className="text-lg font-montserrat font-bold text-pink-600">{projectTitle}</h1>
+      </div>
+        
+      
+         
+    </div>
   )
 }
--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -6,16 +6,7 @@ export const AboutMe = () => {
       <h1 className="mb-16 text-4xl font-bold font-montserrat text-pink-600">About Me</h1>
       <div className=" flex  mx-auto px-5">
         <p className="text-xl font-bold text-pink-700 font-palanquin">
-          <span className="text-6xl font-cursive leading-[0] ">"I</span> am a
-          4th Year B.Tech student at Sharnbasava University Kalaburgi,
-          specializing in Frontend Web Development. With a strong foundation in
-          frontend technologies such as React, and proficiency in backend
-          technologies like Node.js, Express, and Next.js, I have successfully
-          completed several impactful web projects as showcased in my portfolio.
-          My programming skills extend to Java, C, and Python. Eager to apply my
-          technical expertise to address real-world challenges, I am
-          enthusiastic about collaborative teamwork and innovation in
-          problem-solving
+          <span className="text-6xl font-cursive leading-[0] ">"I</span> am a passionate full-stack web developer with strong expertise in Node.js and MongoDB. Recently graduated with a B.Tech in Computer Science Engineering from Sharnbasava University Kalaburgi, I have a robust understanding of modern web development technologies and frameworks. I am committed to building efficient, scalable, and user-friendly web applications. My technical skills, combined with my enthusiasm for continuous learning and problem-solving, make me an asset in any development team
           <span className="font-cursive">"</span>
         </p>
       </div>
